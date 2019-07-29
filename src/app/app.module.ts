7c5de3d128b602ed605707293a93d2181c8dcc12
sandeep singh <sandeep.gwebs@gmail.com>
2019-07-29 17:34:20 +0530

    created new basicnote-native app

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import { NativeScriptModule } from "nativescript-angular/nativescript.module";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+//import { LoginPipe } from './login.pipe';
+
+@NgModule({
+    bootstrap: [
+        AppComponent
+    ],
+    imports: [
+        NativeScriptModule,
+        AppRoutingModule
+    ],
+    declarations: [
+        AppComponent,
+      //      LoginPipe
+    ],
+    schemas: [
+        NO_ERRORS_SCHEMA
+    ]
+})
+export class AppModule { }
