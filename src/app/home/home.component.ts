dfddd3bbb84d12e7d47d4a129a84209f90845234
Vijay <vijay.gwebs@gmail.com>
2019-07-30 17:51:29 +0530

    android https solved

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,16 +11,19 @@ import { User } from "../models/user";
 export class HomeComponent implements OnInit {
     token: any;
     APIURL: string = 'http://basicnote.mandisuppliers.com/api/';
-    user : User
+    user : User;
     constructor(private router: Router, private http: HttpClient) {
         // Use the component constructor to inject providers.
         this.user = new User();
+        this.user.email = ''; 
+        this.user.password = '123'; 
     }
 
     ngOnInit(): void {
         // Init your component properties here.
     }
     login() {
+        alert(JSON.stringify(this.user));
         this.http.post(this.APIURL + 'auth/login',{email:this.user.email, password: this.user.password}).subscribe(data => {
             alert('asd');
             this.router.navigate(['new-note']); 
